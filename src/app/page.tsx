3c978fc6c30329cd6c707f9cd3e5e00a76597535
rrav0016 <rrav0016@student.monash.edu>
2024-04-03 18:37:26 +1100

    improved the animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,84 @@
 "use client";
-import { useState, Suspense } from "react";
-import { motion, useScroll, useMotionValueEvent } from "framer-motion";
+import { useState, useRef, useLayoutEffect } from "react";
+import { motion } from "framer-motion";
 import useMousePosition from "@/utils/useMousePosition";
 import styles from "../styles/page.module.scss";
 import SplineWrapper from "@/components/SplineWrapper";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 export default function Home() {
   const [isHovered, setIsHovered] = useState(false);
 
   const { x, y } = useMousePosition();
 
-  const { scrollYProgress } = useScroll()
-
   const size = isHovered ? 500 : 40;
 
-  useMotionValueEvent(scrollYProgress, "change", (latest: any) => {
-    console.log("Page scroll: ", latest)
-  })
+  const splineContainerRef = useRef(null);
+  const timeline = useRef(null);
+ 
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+  timeline.current = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#part1",
+      start: "middle bottom",
+      end: "bottom bottom",
+      markers: true,
+      scrub: true,
+    }
+  });
+  timeline.current
+  .from(splineContainerRef.current, { opacity: 0})
+  .to(splineContainerRef.current, { opacity: 1, xPercent: 0});
+
+  timeline.current = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#part2",
+      start: "top bottom",
+      end: "bottom bottom",
+      markers: true,
+      scrub: true,
+    }
+  });
+  timeline.current
+  .to(splineContainerRef.current, { xPercent: 25});
+
+  timeline.current = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#part3",
+      start: "top bottom",
+      end: "bottom bottom",
+      markers: true,
+      scrub: true,
+    }
+  });
+  timeline.current
+  .to(splineContainerRef.current, { xPercent: -25});
+  timeline.current = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#part4",
+      start: "top bottom",
+      end: "bottom bottom",
+      markers: false,
+      scrub: true,
+    }
+  });
+  timeline.current
+  .to(splineContainerRef.current, { xPercent: 25, ease: "power2.inOut"});
+  }, []);
+  
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center relative">
       <div className="w-full h-full ">
-        <motion.div className="fixed top-0 left-0 w-dvw h-dvh"
-        initial={{y: "-10%"}}
-        animate={{
-          x:
-          scrollYProgress.get() >= 0.1 && scrollYProgress.get() < 0.2
-            ? `${(scrollYProgress.get() / 0.2) * 0}%`
-            : scrollYProgress.get() >= 0.2 && scrollYProgress.get() < 0.4
-            ? "0%"
-            : scrollYProgress.get() >= 0.4 && scrollYProgress.get() < 0.6
-            ? `${(1 - (scrollYProgress.get() - 0.4) / 0.4) * 25}%`
-            : scrollYProgress.get() >= 0.6 && scrollYProgress.get() < 0.8
-            ? `${(scrollYProgress.get() - 0.6) / 0.2 * -25}%`
-            : scrollYProgress.get() >= 0.8 && scrollYProgress.get() < 0.95
-            ? "-15%"
-            : scrollYProgress.get() >= 0.95
-            ? "25%"
-            : "25%",
-          y:
-          scrollYProgress.get() >= 0.2 ? `0%` : "-10%"
-        }}
-        transition={{ type: "tween", ease: "backOut", duration: 1 }}
+        <div 
+        className="fixed top-0 left-0 w-dvw h-dvh"
+        ref={splineContainerRef}
         >
           <SplineWrapper />
-        </motion.div>
+        </div>
 
         <motion.div
           className={styles.mask}
@@ -119,25 +153,25 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="snap-start h-dvh w-full flex items-center justify-center">
+        <div className="snap-start h-dvh w-full flex items-center justify-center" id="part1">
           <p className="text-9xl font-extrabold text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
             The Journey of an <br />
             Apple
           </p>
         </div>
-        <div className="h-dvh w-full flex items-center justify-center snap-start">
+        <div className="h-dvh w-full flex items-center justify-center snap-start" id="part2">
           <p className="text-9xl font-extrabold text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
             The Journey of an <br />
             Apple
           </p>
         </div>
-        <div className="h-dvh w-full flex items-center justify-center snap-start">
+        <div className="h-dvh w-full flex items-center justify-center snap-start" id="part3">
           <p className="text-9xl font-extrabold text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
             The Journey of an <br />
             Apple
           </p>
         </div>
-        <div className="h-dvh w-full flex items-center justify-center snap-start">
+        <div className="h-dvh w-full flex items-center justify-center snap-start" id="part4">
           <p className="text-9xl font-extrabold text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
             The Journey of an <br />
             Apple
--- src/components/SplineWrapper.jsx
+++ src/components/SplineWrapper.jsx
@@ -1,72 +1,16 @@
-"use client";
-
-import { useRef } from "react";
+import React, { Suspense,  useRef} from "react";
+import LazyLoader from "@/utils/lazyLoader";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import React, { Suspense } from "react";
-import LazyLoader from "@/utils/lazyLoader";
 
 const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 export default function SplineWrapper() {
-  const apple = useRef();
-  const camera = useRef();
-  const tree = useRef();
-  const truck = useRef();
-  const crate = useRef();
 
-  gsap.registerPlugin(ScrollTrigger);
-
-  function onLoad(spline) {
-    apple.current = spline.findObjectByName("Apple_Model");
-    camera.current = spline.findObjectByName("Camera");
-    tree.current = spline.findObjectByName("Tree");
-    truck.current = spline.findObjectByName("Truck");
-    crate.current = spline.findObjectByName("Crate");
-
-    setTimeout(() => {
-      animateApple();
-    }, 4500);
-  }
-
-  function animateApple() {
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: "#part2",
-          start: "top bottom",
-          end: "bottom bottom",
-          scrub: true,
-        },
-      })
-      .to(apple.current.position, {
-        x: apple.current.position.x + 60,
-        y: apple.current.position.y - 20,
-        z: apple.current.position.z + 40,
-        duration: 1,
-        ease: "none",
-      })
-      .to(
-        tree.current.position,
-        {
-          x: tree.current.position.x + 30,
-          y: tree.current.position.y - 5,
-          z: tree.current.position.z + 20,
-          duration: 1,
-          ease: "none",
-        },
-        "<"
-      );
-  }
 
   return (
-    <div>
-      <Suspense fallback={<LazyLoader delay={300} className="h-full w-full" />}>
-        <Spline
-          scene="https://prod.spline.design/DwFAYXJyneWm2UEQ/scene.splinecode"
-          // onLoad={onLoad}
-        />
-      </Suspense>
-    </div>
+    <Suspense fallback={<LazyLoader delay={300}  />}>
+        <Spline scene="https://prod.spline.design/DwFAYXJyneWm2UEQ/scene.splinecode" />   
+    </Suspense>
   );
 }
