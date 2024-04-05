51fa1c1cb7e90dc39123c10dde205791fd520e61
rrav0016 <rrav0016@student.monash.edu>
2024-04-05 16:23:12 +1100

    fixed homepage layout issue

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import diner from "../assets/diner.jpg";
+import diner from "../assets/diner.webp";
 import Image from "next/image";
 
 
