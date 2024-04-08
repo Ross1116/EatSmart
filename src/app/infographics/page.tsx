417e6d8285d2cce83807344bac60971038862961
rrav0016 <rrav0016@student.monash.edu>
2024-04-08 21:03:54 +1000

    added anims info page

--- src/app/infographics/page.tsx
+++ src/app/infographics/page.tsx
@@ -1,22 +1,56 @@
 "use client";
 import React, { Suspense } from "react";
 import Quiz from "@/components/Quiz/QuizWrapper";
+import { motion } from "framer-motion";
 
 const NavBar = React.lazy(() => import("@/components/NavBar"));
 
 export default function Infographics() {
+
+  const stagger = {
+    animate: {
+        transition: {
+            staggerChildren: 0.1,
+            // delayChildren: 0.1,
+        },
+    }
+}
+
+const pageAnimation = {
+  initial: {
+      y: 200,
+      scale: 0.8,
+      opacity: 0,
+  },
+  animate: {
+      y: 0,
+      scale: 1,
+      opacity: 1,
+      transition: {
+          duration: 1.4,
+          ease: "backOut",
+      },
+  },
+}
+
   return (
     <div>
       <Suspense>
         <NavBar />
       </Suspense>
 
-      <div className="w-dvw h-dvh flex flex-col items-center justify-center gap-16">
-      <h1 className="mt-24 text-center text-8xl font-bold">Infographics</h1>
-      <div className="flex items-center justify-center">
-        <Quiz />
-      </div>
-      </div>
+      <motion.div
+      initial="initial"
+      animate="animate"
+      variants={stagger}
+       className="w-dvw h-dvh flex flex-col items-center justify-center gap-16">
+      <motion.h1
+      variants={pageAnimation}
+       className="mt-24 text-center text-8xl font-bold">Infographics</motion.h1>
+       <motion.div variants={pageAnimation}>
+          <Quiz />
+        </motion.div>
+      </motion.div>
 
     </div>
   );
