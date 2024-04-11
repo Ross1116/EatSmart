3e78c045e3d775495d533dd738d2c3bbb8e4f25f
rrav0016 <rrav0016@student.monash.edu>
2024-04-12 07:06:46 +1000

    slight footer changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import { Youtube, Twitter, Github } from "lucide-react";
+// import { Youtube, Twitter, Github } from "lucide-react";
 
 export default function Footer() {
   return (
-    <div className="bg-[#121405] to-transparent py-8 w-dvw z-20 rounded-t-3xl">
-      <div className="container flex flex-col md:flex-row items-center justify-between">
+    <div className="bg-[#121405] to-transparent py-4 w-dvw z-20 rounded-t-3xl">
+      <div className="container flex flex-col md:flex-row items-end justify-between">
         <div className="flex flex-col items-start justify-end text-sm">
           {/* <Link className=" hover:text-gray-300" href="#">
             <Youtube className="h-6 w-6" />
@@ -23,12 +23,12 @@ export default function Footer() {
         </div>
         <p className="mt-4 md:mt-0 text-sm">© 2024 EatSmart Inc. All rights reserved.</p>
       </div>
-      <div className="py-3 w-full px-72 flex flex-col gap-3">
+      <div className="pt-3 w-full px-72 flex flex-col gap-3">
         <div className="h-[1px] w-full bg-background-950"></div>
-        <div className="flex gap-4 pl-1">
-          <p className="hover:underline text-text-950 font-light text-xs">Website Privacy Notice</p>
-          <p className="hover:underline text-text-950 font-light text-xs">Cookies</p>
-          <p className="hover:underline text-text-950 font-light text-xs">Legal</p>
+        <div className="flex gap-4 pl-1 hover:cursor-pointer">
+          <p className="underline hover:no-underline text-text-950 font-light text-xs">Website Privacy Notice</p>
+          <p className="underline hover:no-underline text-text-950 font-light text-xs">Cookies</p>
+          <p className="underline hover:no-underline text-text-950 font-light text-xs">Legal</p>
         </div>
       </div>
     </div>
