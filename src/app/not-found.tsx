3c410473ed768fa0d1cbf4db90f0d74490f64392
rrav0016 <rrav0016@student.monash.edu>
2024-04-11 22:32:22 +1000

    added 404 animations

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,28 +1,74 @@
 "use client";
+
 import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
 import { Button } from "@/components/ui/button";
 import { ArrowUpLeftFromCircle } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 function NotFoundPage() {
   const router = useRouter();
+
+  const stagger = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const pageAnimation = {
+    initial: {
+      y: 200,
+      scale: 0.8,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      scale: 1,
+      opacity: 1,
+      transition: {
+        duration: 1.4,
+        ease: "backOut",
+      },
+    },
+  };
+
   return (
     <div>
       <div>
         <NavBar />
       </div>
-      <div className="h-screen flex flex-col gap-12 justify-center items-center">
-        <h1 className="text-7xl text-center font-extrabold w-1/2">
-          This page is currently under construction please come back later
-        </h1>
-        <Button
-          onClick={() => router.back()}
-          className="bg-primary-500 rounded-3xl text-text-50 flex flex-row justify-center gap-2"
+      <motion.div
+        initial="initial"
+        animate="animate"
+        variants={stagger}
+        className="h-screen flex flex-col gap-12 justify-center items-center"
+      >
+        <motion.h1
+          variants={pageAnimation}
+          className="text-7xl text-center font-extrabold w-1/2"
         >
-          <ArrowUpLeftFromCircle />Go Back
-        </Button>
-      </div>
+          Whoops!
+        </motion.h1>
+        <motion.p
+          variants={pageAnimation}
+          className="text-xl text-center font-semibold w-[35%]"
+        >
+          The page you are looking for does not exist or is under construction.
+          Please go back to the previous page.
+        </motion.p>
+        <motion.div variants={pageAnimation}>
+          <Button
+            onClick={() => router.back()}
+            className="bg-primary-500 rounded-3xl text-text-50 flex flex-row justify-center gap-2 font-semibold hover:text-text-950 hover:bg-background-50 hover:ring-2 hover:ring-background-950"
+          >
+            <ArrowUpLeftFromCircle />
+            Go Back
+          </Button>
+        </motion.div>
+      </motion.div>
       <Footer />
     </div>
   );
